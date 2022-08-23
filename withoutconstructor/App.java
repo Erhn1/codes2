d6faa6252cde615d450693a7b0f3a9307a67fc15
Erhn1 <111546581+Erhn1@users.noreply.github.com>
2022-08-23 12:00:27 +0800

    Create App.java

---
+++ withoutconstructor/App.java
@@ -0,0 +1,38 @@
+public class App {
+    public static void main(String[] args) {
+
+        Character knight = new Character();
+
+        knight.strength = 10;
+        knight.agility = 5;
+        knight.intelligence = 0;
+        knight.name = "Chen";
+        knight.sayMyName();
+
+        Character mage = new Character();
+
+        mage.strength = 0;
+        mage.agility = 5;
+        mage.intelligence = 10;
+        mage.name = "Akasha";
+        mage.sayMyName();
+
+        Character thief = new Character();
+
+        thief.strength = 5;
+        thief.agility = 10;
+        thief.intelligence = 0;
+        thief.name = "Gondar";
+        thief.sayMyName();
+        thief.sayMyAgility();
+        thief.sayMyStrength();
+        thief.sayMyIntelligence();
+
+        Character healer = new Character();
+
+        healer.strength = 9;
+        healer.agility = 9;
+        healer.intelligence = 9;
+        healer.name = "Ming";
+    }
+}
