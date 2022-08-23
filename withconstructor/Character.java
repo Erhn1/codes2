2e6d8f51cf75b097c723ddd73131f263ef2c100a
Erhn1 <111546581+Erhn1@users.noreply.github.com>
2022-08-23 11:59:20 +0800

    Create Character.java

---
+++ withconstructor/Character.java
@@ -0,0 +1,36 @@
+public class Character {
+    // Attributes
+    int strength;
+    int agility;
+    int intelligence;
+    String name;
+
+    // Method
+    public void sayMyName() {
+        System.out.println("MyName is " + name);
+    }
+
+    public void sayMyStrength() {
+        System.out.println("MyStrength is " + strength);
+    }
+
+    public void sayMyAgility() {
+        System.out.println("MyAgility is " + agility);
+    }
+
+    public void sayMyIntelligence() {
+        System.out.println("MyIntelligence is " + intelligence);
+    }
+
+
+
+    // Constructor
+    public Character(int str, int agi, int intel, String characterName) {
+        strength = str;
+        agility = agi;
+        intelligence = intel;
+        name = characterName;
+    }
+
+
+}
